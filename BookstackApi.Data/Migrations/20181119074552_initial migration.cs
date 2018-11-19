a513e16550f5d8a8adfa16ae4e6db96e8fc968c4
Alexander Frolov <big_air2409@mail.ru>
2018-11-19 20:57:35 +1300

    run first migration. The proj starts OK with default controller

---
+++ BookstackApi.Data/Migrations/20181119074552_initial migration.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookstackApi.Data.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    CoverUrl = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    YearPublished = table.Column<int>(nullable: false),
+                    ISBN = table.Column<string>(nullable: true),
+                    BookReview = table.Column<string>(nullable: true),
+                    BookRating = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookComments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Author = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    BookId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookComments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookComments_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    BookId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookTags_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookComments_BookId",
+                table: "BookComments",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTags_BookId",
+                table: "BookTags",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookComments");
+
+            migrationBuilder.DropTable(
+                name: "BookTags");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
